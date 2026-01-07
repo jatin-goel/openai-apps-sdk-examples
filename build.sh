fb50176effb7f43eb519ad692b5e3599d384e3d1
jatin <jating06@gmail.com>
2026-01-07 12:46:58 +0530

    added sh file

---
+++ build.sh
@@ -0,0 +1,15 @@
+sh#!/bin/bash
+
+set -e
+
+# Build widgets
+cd widgets
+pnpm install
+pnpm run build
+
+# Install razorpay_server_node dependencies
+cd ../razorpay_server_node
+pnpm install
+
+echo "Build completed successfully!"
+
