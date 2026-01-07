8a7281f44f1e35b265027401b9e2452d0cbbe47e
jatin <jating06@gmail.com>
2026-01-07 14:53:00 +0530

    updated code

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Enable corepack to use pnpm (specified in package.json packageManager field)
+corepack enable
+
 # Build widgets
 cd widgets
 pnpm install
@@ -12,4 +15,3 @@ cd ../razorpay_server_node
 pnpm install
 
 echo "Build completed successfully!"
-
